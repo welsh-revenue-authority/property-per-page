10c0c5baee10317d3454a8ffa305250ff112ac65
Richard Pope <richard@richardpope.org>
2022-04-06 12:16:21 +0100

    welsh gov pages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 python3 -m venv venv
-venv venv/bin/activate
+source venv/bin/activate
 export FLASK_APP=main
 export FLASK_ENV=development
 flask run --host=0.0.0.0
--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    return render_template('search.html')
