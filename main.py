47d3b42651cd4fec9771a5659b16fc9c15ca44d6
Richard Pope <richard@richardpope.org>
2022-04-10 10:21:43 +0100

    addeed final page

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import helpers
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -12,23 +14,30 @@ def search():
     search_term =  request.args.get("global-keywords")
 
     properties = [
-            {'uprn': 10013705907, 'address': '159, Idwal Street, Upper Leadworth'},
-            {'uprn': 10013705990, 'address': '177, Owain Road, Upper Leadworth'},
-            {'uprn': 10013706214, 'address': '152, Castle Street, Upper Leadworth'},
-            {'uprn': 10013706215, 'address': '15, Mountain Drive, Upper Leadworth'},
-            {'uprn': 10013706216, 'address': '174, Mountain Road, Upper Leadworth'},
-            {'uprn': 10013706218, 'address': '38, Highstreet, Upper Leadworth'},
-            {'uprn': 10013706219, 'address': '133, Pleasant Drive, Upper Leadworth'},
-            {'uprn': 10013706220, 'address': '141, Mountain Drive, Upper Leadworth'},
-            {'uprn': 10013706221, 'address': '90, Idwal Court, Upper Leadworth'},
-            {'uprn': 10013706222, 'address': '32, Idwal Court, Upper Leadworth'},
-            {'uprn': 10013706223, 'address': '178, Pleasant Road, Upper Leadworth'},
-            {'uprn': 10013706224, 'address': '199, Mountain Court, Upper Leadworth'},
-            {'uprn': 10013706225, 'address': '194, Highstreet, Upper Leadworth'},
-            {'uprn': 10013706226, 'address': '100, Owain Court, Upper Leadworth'},
-            {'uprn': 10013706227, 'address': '190, Glyndŵr Street, Upper Leadworth'},
-            {'uprn': 10013706229, 'address': '114, Glyndŵr Bank, Upper Leadworth'},
-            {'uprn': 10013706230, 'address': '107, Glyndŵr Bank, Upper Leadworth'}
+            {'uprn': 45, 'address': '159, Idwal Street, Upper Leadworth, Clwyd'},
+            {'uprn': 46, 'address': '177, Owain Road, Upper Leadworth, Clwyd'},
+            {'uprn': 47, 'address': '152, Castle Street, Upper Leadworth, Clwyd'},
+            {'uprn': 48, 'address': '15, Mountain Drive, Upper Leadworth, Clwyd'},
+            {'uprn': 49, 'address': '174, Mountain Road, Upper Leadworth, Clwyd'},
+            {'uprn': 50, 'address': '38, Highstreet, Upper Leadworth, Clwyd'},
+            {'uprn': 51, 'address': '133, Pleasant Drive, Upper Leadworth, Clwyd'},
+            {'uprn': 52, 'address': '141, Mountain Drive, Upper Leadworth, Clwyd'},
+            {'uprn': 53, 'address': '90, Idwal Court, Upper Leadworth, Clwyd'},
+            {'uprn': 54, 'address': '32, Idwal Court, Upper Leadworth, Clwyd'},
+            {'uprn': 55, 'address': '178, Pleasant Road, Upper Leadworth, Clwyd'},
+            {'uprn': 56, 'address': '199, Mountain Court, Upper Leadworth, Clwyd'},
+            {'uprn': 57, 'address': '194, Highstreet, Upper Leadworth, Clwyd'},
+            {'uprn': 58, 'address': '100, Owain Court, Upper Leadworth, Clwyd'},
+            {'uprn': 59, 'address': '190, Glyndŵr Street, Upper Leadworth, Clwyd'},
+            {'uprn': 60, 'address': '114, Glyndŵr Bank, Upper Leadworth, Clwyd'},
+            {'uprn': 61, 'address': '107, Glyndŵr Bank, Upper Leadworth, Clwyd'}
     ]
 
     return render_template('search.html', search_term=search_term, properties=properties)
+
+@app.route('/property/<int:platform_id>', methods=['GET'])
+def property(platform_id):
+    if isinstance(platform_id, int):
+        response = requests.post('https://land-property-platform.herokuapp.com/property_info', data = json.dumps({'platform_property_id':platform_id}))
+        data = response.json()
+    return render_template('property.html', data=data)
